83f080427edf9050be45c69267791024839cdb97
rail888 <belokana@hanmail.net>
2025-06-11 10:15:10 +0900

    v0

---
+++ src/main/java/pack/Sample08Application.java
@@ -0,0 +1,20 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Sample08Application {
+	@GetMapping("/")
+	public String hello(){
+		return "06/10: hello 시저 v01";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample08Application.class, args);
+	}
+
+}
---
+++ src/test/java/pack/Sample08ApplicationTests.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
